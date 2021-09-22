2648154ea949d203e58a75bea34153a7170e44c4
Gabriel Rocha <knuckles4x@gmail.com>
2021-09-22 11:30:25 -0300

    TextButton, Row, etc

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.red.shade800, fontWeight: FontWeight.bold)
-        ),
-        Text(
-          "Pode Entrar!",
-          style: TextStyle(color: Colors.red.shade800, fontStyle: FontStyle.italic,
-          fontSize: 30.0)
-        ),
-      ],
-    )
-  ));
+      title: "Contador de Pessoas",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Pessoas: 0",
+              style: TextStyle(
+                  color: Colors.red.shade800, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.red,
+                  ),
+                ),
+                onPressed: (() => {}),
+              ),
+              TextButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.red,
+                  ),
+                ),
+                onPressed: (() => {}),
+              ),
+            ],
+          ),
+          Text("Pode Entrar!",
+              style: TextStyle(
+                  color: Colors.red.shade800,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0)),
+        ],
+      )));
 }
