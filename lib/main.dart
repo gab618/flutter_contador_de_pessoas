e78960c7631220165e1d39faf4dfd8494df3b2b3
Gabriel Rocha <knuckles4x@gmail.com>
2021-09-22 11:09:45 -0300

    Column and Text

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Container(color: Colors.white,)
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Pessoas: 0",
+          style: TextStyle(color: Colors.red.shade800, fontWeight: FontWeight.bold)
+        ),
+        Text(
+          "Pode Entrar!",
+          style: TextStyle(color: Colors.red.shade800, fontStyle: FontStyle.italic,
+          fontSize: 30.0)
+        ),
+      ],
+    )
   ));
 }
